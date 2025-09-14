80e3eaf182bf957b10c207ef2a2dad864dece470
Govinda <upadhyagovinda52@gmail.com>
2025-09-14 21:59:23 +0530

    done

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -613,7 +613,7 @@ You can set your browser to refuse all or some browser cookies, or to alert you 
 
       {/* Footer Section */}
       <footer className="bg-gradient-to-r from-green-900 to-emerald-900 text-white mt-auto">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <div className="flex justify-around max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
             {/* Brand */}
             <div className="lg:col-span-1">
